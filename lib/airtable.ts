6e1e6dbedae2ec69339907f84dd3a18e02fd7910
최우석 <choiwooseok@choeuseog-ui-MacBookAir.local>
2026-01-30 16:09:29 +0900

    Fix: Type cast for null in Airtable update

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -351,7 +351,7 @@ export async function updateApplicationCheckin(
                 fields: {
                     '입실일': checkInDate,
                     '입실 사이트': checkInSite,
-                    '예약 취소/변경': null // Single Select 필드 초기화는 null 사용해야 함
+                    '예약 취소/변경': null as any // Single Select 필드 초기화
                 }
             }
         ]);
